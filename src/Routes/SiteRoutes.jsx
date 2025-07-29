abf84c9d192d164198b28a062bf7411ce3675a10
Ani12234 <anirudhkulkarni733@gmail.com>
2025-07-29 22:45:30 +0530

    enchancement in loading of the newsletter

--- src/Routes/SiteRoutes.jsx
+++ src/Routes/SiteRoutes.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import Home from "../components/Home/Home";
 import { Route, Router, Routes } from "react-router-dom"; // Removed HashRouter import
 import UnderConstruction from "../components/UnderConstruction/UnderConstruction";
@@ -6,30 +6,27 @@ import Notfound from "../components/NotFound/Notfound";
 import Execoms from "../components/Execoms/Execoms";
 import ScrollToTop from "../components/NotFound/ScrollToTop";
 import CommitteePage from "../components/CommitteePage";
-const Achievements = lazy(() => import("../components/Achievements/Achievements"));
+import Achievements from "../components/Achievements/Achievements";
 import SocietyPage from "../components/Societies/society-page";
-import Loader from "../components/Loader/Loader";
 const SiteRoutes = () => {
   return (
     <>
       <ScrollToTop />
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<UnderConstruction />} />
-          <Route path="/events" element={<UnderConstruction />} />
-          <Route path="/achievements" element={<Achievements />} />
-          {/* <Route path="/execoms" element={<UnderConstruction />} /> */}
-          <Route path="/societies" element={<SocietyPage />} />
-          <Route path="/affinities" element={<UnderConstruction />} />
-          <Route path="/membership" element={<UnderConstruction />} />
-          <Route path="/photo-gallery" element={<UnderConstruction />} />
-          <Route path="/joinIEEE" element={<UnderConstruction />} />
-          <Route path="/execoms/:team/:year" element={<UnderConstruction />} />
-          <Route path="/execoms" element={<CommitteePage />} />
-          <Route path="/*" element={<Home />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<UnderConstruction />} />
+        <Route path="/events" element={<UnderConstruction />} />
+        <Route path="/achievements" element={<Achievements />} />
+        {/* <Route path="/execoms" element={<UnderConstruction />} /> */}
+        <Route path="/societies" element={<SocietyPage />} />
+        <Route path="/affinities" element={<UnderConstruction />} />
+        <Route path="/membership" element={<UnderConstruction />} />
+        <Route path="/photo-gallery" element={<UnderConstruction />} />
+        <Route path="/joinIEEE" element={<UnderConstruction />} />
+        <Route path="/execoms/:team/:year" element={<UnderConstruction />} />
+        <Route path="/execoms" element={<CommitteePage />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
     </>
   );
 };
